238bd80c48e54a1ce78a9a529337f7b38386068b
Vlad Matulis <matulis@pm.me>
2023-02-25 19:59:11 +0100

    Learn to use http binance api

--- example/websocket-client-sync-ssl.cpp
+++ example/websocket-client-sync-ssl.cpp
@@ -13,18 +13,19 @@
 //
 //------------------------------------------------------------------------------
 #include <chrono>
-#include <thread>
-
-#include <boost/beast/core.hpp>
-#include <boost/beast/ssl.hpp>
-#include <boost/beast/websocket.hpp>
-#include <boost/beast/websocket/ssl.hpp>
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/ssl/stream.hpp>
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <thread>
+
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/stream.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/ssl.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/websocket/ssl.hpp>
 
 namespace beast = boost::beast;         // from <boost/beast.hpp>
 namespace http = beast::http;           // from <boost/beast/http.hpp>
@@ -34,7 +35,7 @@ namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
 // Sends a WebSocket message and prints the response
-int main(int argc, char** argv)
+int main2(int argc, char** argv)
 {
     try
     {
@@ -129,4 +130,95 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
+}
+
+#include <iostream>
+#include <boost/beast.hpp>
+#include <boost/asio/ssl.hpp>
+#include <openssl/hmac.h>
+#include <cstring>
+
+namespace http = boost::beast::http;
+namespace asio = boost::asio;
+namespace ssl = boost::asio::ssl;
+
+#include <openssl/hmac.h>
+#include <sstream>
+#include <iomanip>
+
+// TODO: check if this is standard compliant, cross-platform and UB-friendly
+std::string hmac_sha256(const std::string& key, const std::string& data) {
+    unsigned char hmac_digest[EVP_MAX_MD_SIZE];
+    unsigned int hmac_digest_len = 0;
+
+    HMAC(EVP_sha256(), key.c_str(), key.length(), (unsigned char*)data.c_str(), data.length(), hmac_digest, &hmac_digest_len);
+
+    std::stringstream ss;
+    ss << std::hex << std::setfill('0');
+    for (unsigned int i = 0; i < hmac_digest_len; ++i) {
+        ss << std::setw(2) << static_cast<unsigned int>(hmac_digest[i]);
+    }
+
+    return ss.str();
+}
+
+int main() {
+    std::string api_key = "7yMcbL557D7DR37EpVtt04VlNQTAHALxy9fXdiHQSgurS93iBXb0QsK07X9sBvJg";
+    std::string secret_key = "ZoDHcd36vUiUYjRKPyOPXmY0xYhL79MOnym2XXIurf739febp6ePIuSV5COnXN03";
+    std::string endpoint = "/api/v3/account";
+    std::string base_url = "testnet.binance.vision";
+
+    asio::io_context ioc;
+    ssl::context ctx{ssl::context::tlsv12_client};
+    ssl::stream<asio::ip::tcp::socket> stream{ioc, ctx};
+    asio::ip::tcp::resolver resolver{ioc};
+    auto const results = resolver.resolve(base_url, "443");
+
+    // Set SNI Hostname (many hosts need this to handshake successfully)
+    if (!SSL_set_tlsext_host_name(stream.native_handle(), base_url.c_str())) {
+        boost::beast::error_code ec{static_cast<int>(::ERR_get_error()), boost::asio::error::get_ssl_category()};
+        throw boost::beast::system_error{ec};
+    }
+
+    asio::connect(stream.next_layer(), results.begin(), results.end());
+    stream.handshake(ssl::stream_base::client);
+
+    // Create request
+    http::request<http::string_body> req{http::verb::get, endpoint, 11};
+    req.set(http::field::host, base_url);
+    req.set(http::field::user_agent, "Beast");
+    req.set("X-MBX-APIKEY", api_key);
+
+    // Add query parameters and signature
+    std::string query_string = "timestamp=" + std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
+    std::string signature = hmac_sha256(secret_key.c_str(), query_string);
+    query_string += "&signature=" + signature;
+    std::cout << "URI: " + endpoint + "?" + query_string << std::endl;
+    req.target(endpoint + "?" + query_string);
+
+    http::write(stream, req);
+
+    // Read response
+    // This buffer is used for reading and must be persisted
+    beast::flat_buffer buffer;
+
+    // Declare a container to hold the response
+    http::response<http::dynamic_body> res;
+
+    // Receive the HTTP response
+    http::read(stream, buffer, res);
+
+    // Write the message to standard out
+    std::cout << res << std::endl;
+
+    boost::system::error_code ec;
+    stream.shutdown(ec);
+    if (ec == boost::asio::error::eof || ec == boost::asio::ssl::error::stream_truncated) {
+        ec = {};
+    }
+    if (ec) {
+        throw boost::beast::system_error{ec};
+    }
+
+    return 0;
 }
