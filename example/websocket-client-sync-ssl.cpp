42f1c864986b4a98082005e5b414a56a0d1ec841
Vlad Matulis <matulis@pm.me>
2023-02-26 17:44:44 +0100

    create ugly function for buying

--- example/websocket-client-sync-ssl.cpp
+++ example/websocket-client-sync-ssl.cpp
@@ -15,6 +15,7 @@
 #include <chrono>
 #include <cstdlib>
 #include <iostream>
+#include <regex>
 #include <string>
 #include <thread>
 
@@ -35,22 +36,23 @@ namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
 // Sends a WebSocket message and prints the response
-int main2(int argc, char** argv)
+int getPricesData(/*int argc, char** argv*/)
 {
     try
     {
-        // Check command line arguments.
-        if(argc != 4)
-        {
-            std::cerr <<
-                "Usage: websocket-client-sync-ssl <host> <port> <text>\n" <<
-                "Example:\n" <<
-                "    websocket-client-sync-ssl echo.websocket.org 443 \"Hello, world!\"\n";
-            return EXIT_FAILURE;
-        }
-        std::string host = argv[1];
-        auto const  port = argv[2];
-        auto const  text = argv[3];
+        // // Check command line arguments.
+        // if(argc != 4)
+        // {
+        //     std::cerr <<
+        //         "Usage: websocket-client-sync-ssl <host> <port> <text>\n" <<
+        //         "Example:\n" <<
+        //         "    websocket-client-sync-ssl echo.websocket.org 443 \"Hello, world!\"\n";
+        //     return EXIT_FAILURE;
+        // }
+        // std::string host = argv[1];
+        std::string host { "stream.binance.com" };
+        std::string port { "443" };
+        std::string text { R"({"method":"SUBSCRIBE","params":["btcusdt@ticker"],"id":1})" };
 
         // The io_context is required for all I/O
         net::io_context ioc;
@@ -112,8 +114,17 @@ int main2(int argc, char** argv)
         for (;;) {
             boost::beast::multi_buffer buffer;
             ws.read(buffer);
-            std::cout << boost::beast::make_printable(buffer.data()) << "\n";
-            std::cout << "FOO\n";
+            std::string resp { beast::buffers_to_string(buffer.data()) };
+            std::regex re("\"w\":\"(\\d+\\.\\d+)\"");
+            std::smatch match;
+            if (std::regex_search(resp, match, re)) {
+                double price = std::stod(match[1].str());
+                std::cout << "Price: " << price << std::endl;
+            } else {
+                std::cout << "No match found" << std::endl;
+                std::cout << resp << "\n";
+            }
+            // std::cout << beast::make_printable(buffer.data()) << "\n";
             if (buffer.size() == 0) {
                 break;
             }
@@ -162,7 +173,7 @@ std::string hmac_sha256(const std::string& key, const std::string& data) {
     return ss.str();
 }
 
-int main() {
+void getAccountData() {
     std::string api_key = "7yMcbL557D7DR37EpVtt04VlNQTAHALxy9fXdiHQSgurS93iBXb0QsK07X9sBvJg";
     std::string secret_key = "ZoDHcd36vUiUYjRKPyOPXmY0xYhL79MOnym2XXIurf739febp6ePIuSV5COnXN03";
     std::string endpoint = "/api/v3/account";
@@ -184,7 +195,7 @@ int main() {
     stream.handshake(ssl::stream_base::client);
 
     // Create request
-    http::request<http::string_body> req{http::verb::get, endpoint, 11};
+    http::request<http::empty_body> req{http::verb::get, endpoint, 11};
     req.set(http::field::host, base_url);
     req.set(http::field::user_agent, "Beast");
     req.set("X-MBX-APIKEY", api_key);
@@ -219,6 +230,79 @@ int main() {
     if (ec) {
         throw boost::beast::system_error{ec};
     }
+}
 
-    return 0;
+void buy() {
+    std::string api_key = "7yMcbL557D7DR37EpVtt04VlNQTAHALxy9fXdiHQSgurS93iBXb0QsK07X9sBvJg";
+    std::string secret_key = "ZoDHcd36vUiUYjRKPyOPXmY0xYhL79MOnym2XXIurf739febp6ePIuSV5COnXN03";
+    std::string endpoint = "/api/v3/order";
+    std::string base_url = "testnet.binance.vision";
+
+    asio::io_context ioc;
+    ssl::context ctx{ssl::context::tlsv12_client};
+    ssl::stream<asio::ip::tcp::socket> stream{ioc, ctx};
+    asio::ip::tcp::resolver resolver{ioc};
+    auto const results = resolver.resolve(base_url, "443");
+
+    // Set SNI Hostname (many hosts need this to handshake successfully)
+    if (!SSL_set_tlsext_host_name(stream.native_handle(), base_url.c_str())) {
+        boost::beast::error_code ec{static_cast<int>(::ERR_get_error()), boost::asio::error::get_ssl_category()};
+        throw boost::beast::system_error{ec};
+    }
+
+    asio::connect(stream.next_layer(), results.begin(), results.end());
+    stream.handshake(ssl::stream_base::client);
+
+    // Create request
+    http::request<http::string_body> req{http::verb::post, endpoint, 11};
+    req.set(http::field::host, base_url);
+    req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+    req.set(http::field::content_type, "application/x-www-form-urlencoded");
+    req.set("X-MBX-APIKEY", api_key);
+
+    // Add query parameters and signature
+    std::string query_string = "symbol=BTCUSDT&side=BUY&type=MARKET&quantity=0.001";
+    query_string += "&timestamp=" + std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
+    std::string signature = hmac_sha256(secret_key.c_str(), query_string);
+    query_string += "&signature=" + signature;
+    std::cout << "QUERY STRING: " << query_string << "\n";
+    std::cout << "Target: " << req.target() << "\n";
+    req.target(endpoint + "?" + query_string);
+    // req.body() = query_string;
+
+    std::cout << "BODY: " << req.body() << "\n";
+
+    http::write(stream, req);
+
+    // Read response
+    // This buffer is used for reading and must be persisted
+    beast::flat_buffer buffer;
+
+    // Declare a container to hold the response
+    http::response<http::dynamic_body> res;
+
+    // Receive the HTTP response
+    http::read(stream, buffer, res);
+
+    // Write the message to standard out
+    std::cout << res << std::endl;
+
+    boost::system::error_code ec;
+    stream.shutdown(ec);
+    if (ec == boost::asio::error::eof || ec == boost::asio::ssl::error::stream_truncated) {
+        ec = {};
+    }
+    if (ec) {
+        throw boost::beast::system_error{ec};
+    }
+}
+
+int main(int argc, char** argv) {
+    std::cout << "Account data BEFORE trade:\n";
+    getAccountData();
+
+    buy();
+
+    std::cout << "Account data AFTER trade:\n";
+    getAccountData();
 }
